93d8c39a38de9cc5de5a56e96f6ef81fd49d93bf
IgiranezaMichel <igiramichael@gmail.com>
2023-12-07 14:42:27 +0200

    first commit

---
+++ src/test/java/com/inyangedemo/InyangedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inyangedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InyangedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
